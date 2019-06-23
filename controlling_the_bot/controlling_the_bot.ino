cb6752a4c5e8f2dc3a5b7afc0c4b18dd27b13d2d
Dimitar Stoilov <dks@aeneas.skyaffie.com>
2019-06-23 16:45:47 +0300

    small update

--- controlling_the_bot/controlling_the_bot.ino
+++ controlling_the_bot/controlling_the_bot.ino
@@ -1,37 +1,14 @@
-/*
-   -- New project --
-   
-   This source code of graphical user interface 
-   has been generated automatically by RemoteXY editor.
-   To compile this code using RemoteXY library 2.3.5 or later version 
-   download by link http://remotexy.com/en/library/
-   To connect using RemoteXY mobile app by link http://remotexy.com/en/download/                   
-     - for ANDROID 4.3.1 or later version;
-     - for iOS 1.3.5 or later version;
-    
-   This source code is free software; you can redistribute it and/or
-   modify it under the terms of the GNU Lesser General Public
-   License as published by the Free Software Foundation; either
-   version 2.1 of the License, or (at your option) any later version.    
-*/
-
-//////////////////////////////////////////////
-//        RemoteXY include library          //
-//////////////////////////////////////////////
-
-// RemoteXY select connection mode and include library 
 #define REMOTEXY_MODE__ESP8266WIFI_LIB_POINT
 #include <ESP8266WiFi.h>
 
 #include <RemoteXY.h>
 
-// RemoteXY connection settings 
+
 #define REMOTEXY_WIFI_SSID "RemoteXY"
 #define REMOTEXY_WIFI_PASSWORD "balkanbot"
 #define REMOTEXY_SERVER_PORT 6377
 
 
-// RemoteXY configurate  
 #pragma pack(push, 1)
 uint8_t RemoteXY_CONF[] =
   { 255,5,0,0,0,60,0,8,13,0,
@@ -42,24 +19,19 @@ uint8_t RemoteXY_CONF[] =
   2,0,39,26,22,11,2,26,31,31,
   79,78,0,79,70,70,0 };
   
-// this structure defines all the variables of your control interface 
+
 struct {
 
-    // input variable
-  uint8_t leftForward; // =1 if button pressed, else =0 
-  uint8_t leftBackward; // =1 if button pressed, else =0 
-  uint8_t rightForward; // =1 if button pressed, else =0 
-  uint8_t rightBackward; // =1 if button pressed, else =0 
 
-    // other variable
-  uint8_t connect_flag;  // =1 if wire connected, else =0 
+  uint8_t leftForward; // =1 if button pressed, else =0 
+  uint8_t leftBackward; 
+  uint8_t rightForward; 
+  uint8_t rightBackward; 
+
 
 } RemoteXY;
 #pragma pack(pop)
 
-/////////////////////////////////////////////
-//           END RemoteXY include          //
-/////////////////////////////////////////////
 
 #define PIN_LEFTFORWARD D2
 #define PIN_LEFTBACKWARD D3
@@ -76,8 +48,7 @@ void setup()
   pinMode (PIN_RIGHTFORWARD, OUTPUT);
   pinMode (PIN_RIGHTBACKWARD, OUTPUT);
   
-  // TODO you setup code
-  
+
 }
 
 void loop() 
@@ -89,8 +60,5 @@ void loop()
   digitalWrite(PIN_RIGHTFORWARD, (RemoteXY.rightForward==0)?LOW:HIGH);
   digitalWrite(PIN_RIGHTBACKWARD, (RemoteXY.rightBackward==0)?LOW:HIGH);
   
-  // TODO you loop code
-  // use the RemoteXY structure for data transfer
-
 
 }
